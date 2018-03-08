e704c4ca33f252f0e490072cedec170d89c60ab8
FVukmirovic <37021715+FVukmirovic@users.noreply.github.com>
2018-03-08 16:32:34 +0100

    Create Vowels.java
    Count the vowels from input string

---
+++ Vowels.java
@@ -0,0 +1,16 @@
+public class Vowels {
+
+  public static int getCount(String str) {
+    int vowelsCount = 0;
+
+    char[] chars = str.toCharArray();
+    for (char c : chars) {
+		if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+			vowelsCount++;
+		}
+	}
+    
+    return vowelsCount;
+  }
+
+}
